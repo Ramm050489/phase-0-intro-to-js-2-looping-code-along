a22d2b78cb07225e78d4452f18ef4a39f7450d41
Ramon <ramm050489@gmail.com>
2021-05-13 00:46:22 -0400

    commit tree 1dc621966ecf73433a28e69fdf8d71089f98764e

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solutions in this file
+// for (let age = 30; age < 40; age++) {
+//     console.log (`I'm ${age} years old. Happy birthday to me!`);
+//     //debugger;
+// }
+
+// const gifts = ['teddy bear', 'drone', 'doll'];
+
+// function wrapGifts(gifts) {
+//     for (let i = 0; i < gifts.lenght; i++) {
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`)
+//         //debugger;
+//     }
+//     return gifts;
+// }
+
+// wrapGifts(gifts);
+
+function writeCards(name, holiday) {
+    let thankyouCards = []
+    for (let i = 0; i < name.length; i++ ) {
+        thankyouCards.push(`Thank you, ${name[i]}, for the wonderful ${holiday} gift!`)
+    }
+    return thankyouCards
+}
+
+function countDown(number) {
+    while (number > 0) {
+        console.log(number)
+        number -= 1
+    }
+    console.log(number)
+
+}
